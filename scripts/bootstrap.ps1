3b162705522b3f88fb1399183cbd817e19f1c0dc
a-mcf <mcfarland.alex@gmail.com>
2016-05-15 20:43:40 -0400

    sql server install working

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -3,8 +3,10 @@
 #cinst powershell -y
 
 Write-Host "Installing SQL Express 2008 R2"
-cinst sqlserver2008r2express-engine -y
+#cinst sqlserver2008r2express-engine -y
 
 # this has a .net 3.5 dependency.
 cinst sqlserver2008r2express-managementstudio -y
-#cinst sqlserver2012express-managementstudio
+#$sqlInstallArgs = ' /INSTANCENAME=MSSQLSERVER /SECURITYMODE=SQL /SAPWD=''vagrant'' /BROWSERSVCSTARTUPTYPE=Automatic /SQLSVCSTARTUPTYPE=Automatic /SQLSVCACCOUNT=''NT AUTHORITY\Network Service'' /SQLSYSADMINACCOUNTS=''BUILTIN\ADMINISTRATORS'' /AGTSVCACCOUNT=''NT AUTHORITY\Network Service'''
+$sqlInstallArgs = ' /ConfigurationFile=c:\vagrant\sqlconfig.ini'
+choco install --force sqlserver2008r2express-engine -installargs $sqlInstallArgs -y

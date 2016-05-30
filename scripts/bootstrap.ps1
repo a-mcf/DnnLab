45673c6d7805b2d2ad30c5c409b966487354a5ef
a-mcf <mcfarland.alex@gmail.com>
2016-05-30 17:55:36 -0400

    cleaned up boostrap file

--- scripts/bootstrap.ps1
+++ scripts/bootstrap.ps1
@@ -1,8 +1,6 @@
-Write-Host "Installing SQL Express 2008 R2"
+Write-Host "Installing SQL Express 2008 R2 / .Net Framework 3.5"
 $sqlInstallArgs = ' /ConfigurationFile=c:\vagrant\sqlconfig.ini'
 choco install sqlserver2008r2express-engine -installargs $sqlInstallArgs -y
-
-# this has a .net 3.5 dependency.
 cinst sqlserver2008r2express-managementstudio -y
 
 # DSC modules
@@ -14,7 +12,11 @@ Install-Module -Name xWebadministration
 Install-Module -Name cNtfsAccessControl
 Install-Module -Name xSQLServer
 
-# dnn stuff
-if (!(Test-Path 'c:\vagrant\dnn\')) { mkdir 'c:\vagrant\dnn\' | Out-Null }
-$dlUrl = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=dotnetnuke&DownloadId=1493875&FileTime=130885394216030000&Build=21031'
-(New-Object System.Net.WebClient).DownloadFile($dlUrl,"c:\vagrant\dnn\dnnInstall.zip")
+# download DNN install file if it does not exist
+if (!(Test-Path -Path "c:\vagrant\dnn\dnnInstall.zip"))
+{
+    if (!(Test-Path 'c:\vagrant\dnn\')) { mkdir 'c:\vagrant\dnn\' | Out-Null }
+    $dlUrl = 'http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=dotnetnuke&DownloadId=1493875&FileTime=130885394216030000&Build=21031'
+    Write-Host "Downloading DNN from $dlUrl"
+    (New-Object System.Net.WebClient).DownloadFile($dlUrl,"c:\vagrant\dnn\dnnInstall.zip")
+}

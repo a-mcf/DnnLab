d834dbad62e5d62a5f00e9359e54e908d52eadbe
Alex McFarland <mcfarland.alex@gmail.com>
2016-10-10 17:17:27 -0400

    Added script to parse installs. Not called yet.

---
+++ scripts/parseDnn.ps1
@@ -0,0 +1,61 @@
+﻿function parseDNN
+{
+    param(
+        [String]
+        $Path = 'C:\users\amcfa\source\bitbucket\DnnLab\dnn\installLog.html',
+        
+        [Switch]
+        $ShowNoMatch
+    )
+
+    $generalRex = "(?<time>\d{2}:\d{2}:\d{2}.\d{3})\s-(&nbsp;)*(?<action>[\w\.\s_-]*)(:)?(\s*)?(?<item>[\w\.\s_-]*)&nbsp;<font color='\w*'>(?<status>\w*)</font>"
+    $packageRex = "(?<time>\d{2}:\d{2}:\d{2}.\d{3})\s-(&nbsp;)*(?<action>Installing Package File)(\s*)?(?<item>[\w\.\s_-]*)(:\s)?&nbsp;<font color='\w*'>(?<status>\w*)</font>"
+
+    (Get-Content $Path) -split '<br>' | ForEach-Object {
+        if ($matches) { $matches.clear() }
+        
+        $matchFound = $_ -match $generalRex
+
+        if ($matchFound)
+        {
+            if ($matches['action'] -like "*Package File*")
+            {
+                $matches.Clear()
+                $_ -match $packageRex |
+                    Out-Null
+                
+                [PSCustomObject] @{
+                    Time = $matches['time']
+                    Action = $matches['action']
+                    Item = $matches['item']
+                    Status = $matches['status']
+                }
+            }
+            else
+            {
+                [PSCustomObject] @{
+                    Time = $matches['time']
+                    Action = $matches['action']
+                    Item = $matches['item']
+                    Status = $matches['status']
+                }
+            }
+        }
+        else
+        {
+            if ($ShowNoMatch)
+            {
+                [PSCustomObject] @{
+                    Time = "no match"
+                    Action = $_
+                    Item = "no match"
+                    Status = "no match"
+                }
+            }
+        }
+    }
+}
+
+$output = parseDNN
+
+$output
